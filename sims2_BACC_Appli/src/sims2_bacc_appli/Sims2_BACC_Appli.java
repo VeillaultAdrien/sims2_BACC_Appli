79d00c49d795a886b8caf91556a4edc32a2b86ac
VeillaultAdrien <veillault.adrien@gmail.com>
2017-09-21 12:09:36 +0200

    ajout du fichier UML

--- sims2_BACC_Appli/src/sims2_bacc_appli/Sims2_BACC_Appli.java
+++ sims2_BACC_Appli/src/sims2_bacc_appli/Sims2_BACC_Appli.java
@@ -21,22 +21,7 @@ public class Sims2_BACC_Appli extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+        Scene scene = new Scene();
         primaryStage.setScene(scene);
         primaryStage.show();
     }
